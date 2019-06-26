3f6e681d86b9bb0ad8c62fbb4283db4d80da34e1
grigory-lapshin <30339972+sadsaviour@users.noreply.github.com>
2019-06-26 21:05:12 +0300

    feat: thnx page layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
+import { Container } from '@material-ui/core';
 import Shelf from './Pages/Shelf';
 import Cart from './Pages/Cart';
 import Checkout from './Pages/Checkout';
@@ -9,10 +10,12 @@ import Thnx from './Pages/Thnx';
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Shelf} />
-      <Route path="/cart/" component={Cart} />
-      <Route path="/checkout/" component={Checkout} />
-      <Route path="/thnx/" component={Thnx} />
+      <Container maxWidth="sm">
+        <Route path="/" exact component={Shelf} />
+        <Route path="/cart/" component={Cart} />
+        <Route path="/checkout/" component={Checkout} />
+        <Route path="/thnx/" component={Thnx} />
+      </Container>
     </Router>
   );
 }
--- src/Pages/Thnx.js
+++ src/Pages/Thnx.js
@@ -1,3 +1,35 @@
 import React from 'react';
+import { styled } from '@material-ui/styles';
+import { Button, Container } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
-export default () => <></>;
+const CenteredContainer = styled(Container)({
+  padding: '6em',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+});
+
+const Header = styled('h1')({
+  textAlign: 'center',
+  marginBottom: '2em',
+});
+
+const BackButton = styled(Button)({
+  background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+  border: 0,
+  borderRadius: 3,
+  boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+  color: 'white',
+  height: 48,
+  padding: '0 30px',
+});
+
+export default () => (
+  <CenteredContainer>
+    <Header>Thank you!</Header>
+    <Link to="/">
+      <BackButton>Back to store</BackButton>
+    </Link>
+  </CenteredContainer>
+);
