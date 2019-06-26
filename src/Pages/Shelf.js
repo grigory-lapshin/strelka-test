504c82fc864a6741b75f37b4057bdf74f7aad7f9
grigory-lapshin <30339972+sadsaviour@users.noreply.github.com>
2019-06-26 22:41:33 +0300

    feat: shelf layout

--- src/Pages/Shelf.js
+++ src/Pages/Shelf.js
@@ -1,25 +1,84 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import Container from '@material-ui/core/Container';
-import { Link } from 'react-router-dom';
-import logo from '../logo.svg';
-import '../App.css';
+import { styled } from '@material-ui/core/styles';
+import { Card } from '@material-ui/core';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GradientButton from '../components/GradientButton';
+import products from '../assets/products.json';
+
+// const products = JSON.parse(productsRaw);
+
+const Header = styled('div')({
+  width: '100%',
+  display: 'flex',
+  flexFlow: 'no-wrap',
+  justifyContent: 'space-between',
+});
+
+const ShopName = styled('h1')({});
+
+const Shelf = styled(GridList)({ width: '100%' });
+
+const ItemCard = styled(Card)({
+  margin: '4px',
+  padding: '1em',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  height: '90%',
+});
+
+const AddButton = styled(GradientButton)({ flexShrink: 0 });
+
+const ProductName = styled('h2')({});
+
+const Description = styled('p')({});
+
+const CardTopHalfContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-start',
+});
+
+const CardBottomHalfContainer = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'flex-end',
+});
+
+const Price = styled('p')({ '&:before': { content: '"$"' }, alignSelf: 'flex-end' });
+
+const GridCard = styled(GridListTile)({});
+
+const Item = ({
+  id, product, description, price,
+}) => (
+  <ItemCard>
+    <CardTopHalfContainer>
+      <ProductName>{product}</ProductName>
+      <Description>{description}</Description>
+    </CardTopHalfContainer>
+    <CardBottomHalfContainer>
+      <Price>{price}</Price>
+      <AddButton>Buy</AddButton>
+    </CardBottomHalfContainer>
+  </ItemCard>
+);
+
+const Cart = items => <div>Cart</div>;
 
 export default () => (
-  <Container maxWidth="md">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </header>
-  </Container>
+  <>
+    <Header>
+      <ShopName>SHOP</ShopName>
+      <Cart />
+    </Header>
+    <Shelf cols={2} spacing={4} cellHeight={420}>
+      {products.map(p => (
+        <GridCard cols={1}>
+          <Item {...p} />
+        </GridCard>
+      ))}
+    </Shelf>
+  </>
 );
